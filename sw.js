0f60c3c519ae057f3b8b4d4ab35369654afbf445
gainwell2024-prog <gainwell2024@gmail.com>
2026-01-11 14:59:45 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'vv-cache-v1';
+const assets = ['./', './index.html', './manifest.json', './bg.png'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
